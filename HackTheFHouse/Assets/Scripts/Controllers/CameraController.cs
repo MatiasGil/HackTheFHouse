8d09e6e1a81c3322e8573db0772011973724838e
Gianti Cristian <gianticristian@gmail.com>
2018-01-27 12:04:25 -0300

    La camara sigue a un objeto determinado con un movimiento suave

---
+++ HackTheFHouse/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private int speed;
+    private Vector3 tempPosition;
+	
+	void Update ()
+    {
+        tempPosition = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        tempPosition.z = -10;
+        transform.position = tempPosition;
+	}
+}
