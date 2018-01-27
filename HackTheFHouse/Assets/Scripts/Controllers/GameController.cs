456603e72a18f716403135c82ff754163141b2e1
Gianti Cristian <gianticristian@gmail.com>
2018-01-27 11:42:14 -0300

    Se agrega un efecto de Fade entre escenas al UIController

--- HackTheFHouse/Assets/Scripts/Controllers/GameController.cs
+++ HackTheFHouse/Assets/Scripts/Controllers/GameController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class GameController : MonoBehaviour 
 {
     public static GameController instance;
     public static GameController Instance { get { return instance; } }
+
 
     private void Awake()
     {
@@ -22,21 +24,29 @@ public class GameController : MonoBehaviour
 
 	public void Play ()
 	{
+        StartCoroutine(UIController.instance.Fade(false));
         SceneManager.LoadScene("Home");
-	}
+        StartCoroutine(UIController.instance.Fade(true));
+    }
 
     public void Exit()
     {
+        StartCoroutine(UIController.instance.Fade(false));
         Application.Quit();
     }
 
     public void MainMenu ()
     {
+        StartCoroutine(UIController.instance.Fade(false));
         SceneManager.LoadScene("Main");
+        StartCoroutine(UIController.instance.Fade(true));
     }
 
     public void Credits()
     {
+        StartCoroutine(UIController.instance.Fade(false));
         SceneManager.LoadScene("Credits");
+        StartCoroutine(UIController.instance.Fade(true));
     }
+
 }
---
+++ HackTheFHouse/Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public static UIController Instance { get { return instance; } }
+
+    private Animator animator;
+    [SerializeField]
+    private float fadeDuration = 1.0f;
+    [SerializeField]
+    private Image fade;
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        animator = GetComponent<Animator>();
+    }
+
+    public void ButtonPlay()
+    {
+        GameController.instance.Play();
+    }
+
+    public void ButtonExit()
+    {
+        GameController.instance.Exit();
+    }
+
+    public void ButtonMainMenu()
+    {
+        GameController.instance.MainMenu();
+    }
+
+    public void ButtonCredits()
+    {
+        GameController.instance.Credits();
+    }
+
+    public IEnumerator Fade(bool fadeAway)
+    {
+        if (fadeAway)
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                fade.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+        else
+        {
+            fade.raycastTarget = true;
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                fade.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+            fade.raycastTarget = false;
+        }
+    }
+
+}
